6137d68fc57b2242de128f848819129e8e9f02a2
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2023-02-07 22:46:07 +0330

    add exit property

--- RockPaperScissors/main.go
+++ RockPaperScissors/main.go
@@ -7,14 +7,16 @@ import (
 
 func main() {
 	fmt.Print("Hello Welcome to Rock-Paper-Scissors Game :)\n\n")
-	fmt.Print("If you want to start the game, write the word `start` : ")
-	var started string
-	fmt.Scan(&started)
-	if started == "start" {
+	fmt.Print("please choice `start` or `exit` : ")
+	var input string
+	fmt.Scan(&input)
+	if input == "start" {
 		fmt.Print("\n\n-_- well down the game is start -_-\n\n")
 		RockPaperScissors()
+	} else if input == "exit" {
+		fmt.Println("--- EXIT ---")
 	} else {
-		fmt.Print("\n\n\n---_--- End Game ---_---\n\n")
+		fmt.Print("\n--- EXIT ---\n")
 		fmt.Print("\nInvalid input Please enter a valid input\n")
 	}
 }
